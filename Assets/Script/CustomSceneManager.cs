2587664d16f22338c3e5aafde86d33546b7c9dd9
math-pixel <59537610+math-pixel@users.noreply.github.com>
2025-05-07 16:30:45 +0200

    Feat(scene) Create Main menu

---
+++ Assets/Script/CustomSceneManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(String sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
