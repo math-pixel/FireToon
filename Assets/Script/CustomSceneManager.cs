624cdc66f325972207d73866ba56b396e7371f1e
math-pixel <59537610+math-pixel@users.noreply.github.com>
2025-05-08 19:03:45 +0200

    Update MainMenu

--- Assets/Script/CustomSceneManager.cs
+++ Assets/Script/CustomSceneManager.cs
@@ -9,7 +9,7 @@ public class CustomSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
