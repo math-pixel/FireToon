e17f12cea998999ef94db869e3bf1d68be387505
math-pixel <mathcb74@gmail.com>
2025-06-15 00:55:04 +0200

    Update UI

--- Assets/Script/EndingManager.cs
+++ Assets/Script/EndingManager.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine;
 
 public class EndingManager : MonoBehaviour
 {
+
+    public RectTransform ReplayButton;
+    public Vector3 ButtonEndPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        ReplayButton.DOAnchorPos(ButtonEndPosition, 2f)
+            .SetEase(Ease.OutElastic)
+            .SetDelay(2f)
+            .OnComplete(() =>
+            {
+                ReplayButton.DOScale(1.5f, 5f)
+                    .SetLoops(-1, LoopType.Yoyo);
+            });
     }
 
     // Update is called once per frame
